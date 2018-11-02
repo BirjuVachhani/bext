5b6102a617c98031d2efbdc50cd5c1dccb5a0930
BirjuVachhani <brvachhani@gmail.com>
2018-11-03 00:56:37 +0530

    feat: add EditText extensions
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

---
+++ bext/src/main/java/com/bext/EditTextExtensions.kt
@@ -0,0 +1,74 @@
+package com.bext
+
+import android.text.Editable
+import android.text.TextUtils
+import android.text.TextWatcher
+import android.widget.EditText
+
+/**
+ * Created by Birju Vachhani on 01-11-2018.
+ */
+
+fun EditText.textString(): String {
+    return this.text.toString()
+}
+
+/*  */
+fun EditText.toInt(default: Int = 0): Int {
+    return this.textString().toIntOrNull() ?: default
+}
+
+fun EditText.toFloat(default: Float = 0.0f): Float {
+    return this.textString().toFloatOrNull() ?: default
+}
+
+fun EditText.toDouble(default: Double = 0.0): Double {
+    return this.textString().toDoubleOrNull() ?: default
+}
+
+fun EditText.toLong(default: Long = 0): Long {
+    return this.textString().toLongOrNull() ?: default
+}
+
+/* Extension for TextChangedListener */
+fun EditText.textWatcher(textChangedBuilderFunc: TextWatcherBuilder.() -> Unit) {
+    val textWatcherBuilder = TextWatcherBuilder().apply {
+        textChangedBuilderFunc()
+    }
+    this.addTextChangedListener(object : TextWatcher {
+        override fun afterTextChanged(e: Editable?) {
+            textWatcherBuilder.after(e)
+        }
+
+        override fun beforeTextChanged(text: CharSequence?, start: Int, count: Int, after: Int) {
+            textWatcherBuilder.before(text, start, count, after)
+        }
+
+        override fun onTextChanged(text: CharSequence?, start: Int, before: Int, count: Int) {
+            textWatcherBuilder.onChanged(text, start, before, count)
+        }
+    })
+}
+
+/* Extension for EditorActionListener */
+fun EditText.imeActionListener(action: Int, consume: Boolean = false, func: (text: String) -> Unit) {
+    this.setOnEditorActionListener { v, actionId, event ->
+        if (actionId == action) {
+            func(this.textString())
+        }
+        return@setOnEditorActionListener consume
+    }
+}
+
+/* check if the text is a valid email address or not */
+fun EditText.isValidEmail(): Boolean {
+    if (TextUtils.isEmpty(this.textString())) return false
+    return android.util.Patterns.EMAIL_ADDRESS.matcher(this.textString()).matches()
+}
+
+/* Used for building TextWatcher */
+class TextWatcherBuilder {
+    var after: (editable: Editable?) -> Unit = { _ -> }
+    var before: (text: CharSequence?, start: Int, count: Int, after: Int) -> Unit = { _, _, _, _ -> }
+    var onChanged: (text: CharSequence?, start: Int, before: Int, count: Int) -> Unit = { _, _, _, _ -> }
+}
