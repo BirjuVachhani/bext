e49ed49a98f99beb56c36105e3918c3eb9a4adc9
BirjuVachhani <brvachhani@gmail.com>
2018-12-16 11:51:56 +0530

    feat: refactor documentation refactor: remove redundant parenthesis feat: add more extensions feat: implement detekt for code quality
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

--- bext/src/main/java/com/bext/ActivityExtensions.kt
+++ bext/src/main/java/com/bext/ActivityExtensions.kt
@@ -25,30 +25,25 @@ import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 
-
 /**
  * Created by Birju Vachhani on 13/11/18.
  */
 
 /**
  * Extension function to hide soft keyboard
- *
  * */
-fun Activity.hideKeyboard() {
-    Handler().post {
-        (getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)?.let {
-            it.hideSoftInputFromWindow(this.findViewById<View>(android.R.id.content).windowToken, 0)
-        }
-    }
+fun Activity.hideKeyboard() = Handler().post {
+    (getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager)
+        ?.hideSoftInputFromWindow(
+            this.findViewById<View>(android.R.id.content).windowToken, 0
+        )
 }
 
 /**
- * Extension function to start/launch new activity
- *
- * @param func is a lambda function with Intent's receiver which provides a lambda block
- * to perform action on to the Intent which will be used to start Activity
- *
- * T takes name of the destination Activity
+ * Extension function to start/launch new [Activity]
+ * @param func is a lambda function with [Intent]'s receiver which provides a lambda block
+ * to perform action on to the [Intent] which will be used to start Activity
+ * T takes name of the destination [Activity]
  * */
 inline fun <reified T : Activity> Activity.navigateTo(func: Intent.() -> Unit = {}) {
     val intent = Intent(this, T::class.java)
@@ -57,14 +52,18 @@ inline fun <reified T : Activity> Activity.navigateTo(func: Intent.() -> Unit = 
 }
 
 /**
- * Extension function to perform Fragment transaction in Activity
- *
- * @param func is a lambda function with FragmentTransaction's receiver which provides access
- * to the transaction instance which will be used to process FragmentTransaction
- *
+ * Extension function to start/launch new [Activity] without passing any data
  * */
-fun AppCompatActivity.transact(func: FragmentTransaction.() -> Unit) {
+inline fun <reified T : Activity> Activity.navigateTo() =
+    this.startActivity(Intent(this, T::class.java))
+
+/**
+ * Extension function to perform [Fragment] transaction in [Activity]
+ * @param func is a lambda function with [FragmentTransaction]'s receiver
+ * which provides access to the transaction instance which will be
+ * used to process [FragmentTransaction]
+ * */
+fun AppCompatActivity.transact(func: FragmentTransaction.() -> Unit) =
     this.supportFragmentManager.beginTransaction().apply {
         func()
     }.commit()
-}
