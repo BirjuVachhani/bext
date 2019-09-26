34597129c7ee7c3a912f9260ac4334b5842a650d
BirjuVachhani <brvachhani@gmail.com>
2019-09-26 12:24:57 +0530

    :arrow_up: Upgrade min sdk version to 19 for all the modules :sparkles: Add new bext-all module to include all the modules in one module :bug: Fix resource not found for content view
    Signed-off-by: BirjuVachhani <brvachhani@gmail.com>

--- bext-core/src/main/java/com/bext/core/ActivityExtensions.kt
+++ bext-core/src/main/java/com/bext/core/ActivityExtensions.kt
@@ -24,7 +24,6 @@ import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
-import com.bext.R
 
 /*
  * Created by Birju Vachhani on 13 November 2018
